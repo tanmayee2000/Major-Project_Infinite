3aaafff186a6cc9748ef165921a28ea38914a83d
Tanmayee Jollu <tanmayeej@infinite.com>
2024-08-28 03:06:24 +0530

    Final Project

---
+++ ETradingSystem/UnitTesting/AdminLoginTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using ETradingSystem.Models;
+using ETradingSystem.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTesting
+{
+    [TestFixture]
+    public class AdminLoginTest
+    {
+        [Test]
+        public void Login_ValidAdmin()
+        {
+            // Arrange
+            var controller = new AdminLoginTest();
+            var email = "raghu@gmail.com";
+            var password = "password";
+
+            // Act
+            var result = controller.Login(email, password) as RedirectToRouteResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("Dashboard", result.RouteValues["controller"]);
+        }
+
+        [Test]
+        public void Login_InvalidAdmin()
+        {
+            // Arrange
+            var controller = new AdminLoginTest();
+            var email = "raghu@gmail.com";
+            var password = "wrongpassword";
+
+            // Act
+            var result = controller.Login(email, password) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Login", result.ViewName);
+            Assert.AreEqual("Invalid Admin Email or Password.", result.ViewBag.InvalidLogin);
+        }
+    }
+}
---
+++ ETradingSystem/UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d47717f0-5045-47f1-a52c-80c20671ec77")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
